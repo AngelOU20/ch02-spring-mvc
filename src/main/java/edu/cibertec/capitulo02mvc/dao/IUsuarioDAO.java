7e65acec14ad30d03064886ca9da452b52034bf5
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-29 23:47:13 -0500

    add interface to UsuarioDAO

---
+++ src/main/java/edu/cibertec/capitulo02mvc/dao/IUsuarioDAO.java
@@ -0,0 +1,11 @@
+package edu.cibertec.capitulo02mvc.dao;
+
+import edu.cibertec.capitulo02mvc.model.UsuarioDTO;
+
+import java.util.List;
+
+public interface IUsuarioDAO {
+    void insertarUsuario(UsuarioDTO usuario);
+    List<UsuarioDTO> listarUsuarios();
+    UsuarioDTO validarLogin(UsuarioDTO usuario);
+}
