5827c5671d849315763d11c103ff8509715b078f
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-29 23:49:11 -0500

    add interface usuarioService

---
+++ src/main/java/edu/cibertec/capitulo02mvc/service/IUsuarioService.java
@@ -0,0 +1,11 @@
+package edu.cibertec.capitulo02mvc.service;
+
+import edu.cibertec.capitulo02mvc.model.UsuarioDTO;
+
+import java.util.List;
+
+public interface IUsuarioService {
+    UsuarioDTO validarLogin(UsuarioDTO usuario);
+    void insertarUsuario(UsuarioDTO usuario);
+    List<UsuarioDTO> listarUsuarios();
+}
