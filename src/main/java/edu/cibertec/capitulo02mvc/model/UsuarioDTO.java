1499d1b0c7c59673b0b252fe6110bf507fa0da25
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-29 23:49:42 -0500

    add usuario dto

---
+++ src/main/java/edu/cibertec/capitulo02mvc/model/UsuarioDTO.java
@@ -0,0 +1,50 @@
+package edu.cibertec.capitulo02mvc.model;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class UsuarioDTO {
+
+    @Size(min = 3, max = 20)
+    private String usuario;
+
+    @NotNull
+    @NotBlank
+    private String clave;
+    private String nombreCompleto;
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(String usuario, String clave, String nombreCompleto) {
+        this.usuario = usuario;
+        this.clave = clave;
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+}
